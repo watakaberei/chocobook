c6a477c600d49b93ed3744fb0f210d52fd30adf4
EC2 Default User <ec2-user@ip-172-31-30-105.ap-northeast-1.compute.internal>
2023-05-21 10:02:16 +0000

    会員情報の作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,23 @@
 class Public::CustomersController < ApplicationController
+  
+  def show
+    @customer = current_customer
+  end
+  
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to customers_my_page_path(current_customer)
+    end
+  end
+
+  private
+  # ストロングパラメータ
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :user_name, :email, :encrypted_password)
+  end
 end
