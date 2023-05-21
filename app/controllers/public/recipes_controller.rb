17492edde47eabe524c6590df6ff1ee38e7ea73c
EC2 Default User <ec2-user@ip-172-31-30-105.ap-northeast-1.compute.internal>
2023-05-21 13:42:04 +0000

    レシピ実装start

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -1,2 +1,25 @@
 class Public::RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+    recipe.save
+    redirect_to '/recipes'
+  end
+
+  def show
+  end
+
+  private
+  # ストロングパラメータ
+  def recipe_params
+    params.require(:recipe).permit(:customer_id, :category_id, :name, :introduction, :cooktime, :image, :material, :quantity, :procedure)
+  end
 end
