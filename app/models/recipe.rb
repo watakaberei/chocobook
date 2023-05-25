b5e97dcc551fb80c8795ecaa706618dfe2ae840c
EC2 Default User <ec2-user@ip-172-31-30-105.ap-northeast-1.compute.internal>
2023-05-25 14:02:23 +0000

    検索機能の追加

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ApplicationRecord
   has_one_attached :image
+  
+  belongs_to :customer
+
+ def self.search(search_word) #①
+  Recipe.where(["name LIKE(?) OR introduction LIKE(?) OR procedure LIKE(?)", #②
+                 "%#{search_word}%", "%#{search_word}%", "%#{search_word}%"])
+ end
+  
 end
