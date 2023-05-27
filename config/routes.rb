b6282683b78af1d867ee167e0ba4a0e6d81dd978
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-05-27 03:24:22 +0000

    更新

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,15 @@ devise_for :customers,skip: [:passwords], controllers: {
     get '/customers/my_page' => 'customers#show'
     get '/customers/information/edit' => 'customers#edit'
     patch '/customers/information/edit' => 'customers#update'
+    get '/customers/unsubscribe' => 'customers#unsubscribe'
+    patch '/customers/withdraw' => 'customers#withdraw'
 
+    resources :recipes, only: [:new, :index, :create, :show, :edit, :update, :destroy] do
+      resource :recipe_bookmarks, only: [:create, :destroy]
+    end
+    get '/search' => 'recipes#search'
+
+    resources :categories, only: [:show]
   end
 
 # 管理者用
@@ -25,6 +33,7 @@ devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
 }
 
   namespace :admin do
+    root to: 'homes#top'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
