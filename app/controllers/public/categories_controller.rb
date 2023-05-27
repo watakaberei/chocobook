61db388d85d19c3249c2d01510ae0f3c281f8cb9
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-05-27 01:38:25 +0000

    タグ検索の実装開始

--- app/controllers/public/categories_controller.rb
+++ app/controllers/public/categories_controller.rb
@@ -1,2 +1,13 @@
 class Public::CategoriesController < ApplicationController
+  
+  def show
+    @category = Category.find(params[:id])  
+  end
+
+   private
+  # ストロングパラメータ
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
+
