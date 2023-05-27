6e5b4ffdd06f4a99e0e78c2efb8a80b36bca36dd
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-05-27 03:22:38 +0000

    いいね機能実装

--- app/controllers/public/recipe_bookmarks_controller.rb
+++ app/controllers/public/recipe_bookmarks_controller.rb
@@ -1,2 +1,17 @@
 class Public::RecipeBookmarksController < ApplicationController
+
+  def create
+    recipe = Recipe.find(params[:recipe_id])
+    recipe_bookmark = current_customer.recipe_bookmarks.new(recipe_id: recipe.id)
+    recipe_bookmark.save
+    redirect_to recipe_path(recipe)
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:recipe_id])
+    recipe_bookmark = current_customer.recipe_bookmarks.find_by(recipe_id: recipe.id)
+    recipe_bookmark.destroy
+    redirect_to recipe_path(recipe)
+
+  end
 end
