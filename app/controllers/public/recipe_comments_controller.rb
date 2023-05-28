3774e357db889dcb41243738f4303768694832d9
EC2 Default User <ec2-user@ip-172-31-30-105.ap-northeast-1.compute.internal>
2023-05-28 13:29:46 +0000

    コメント機能の実装開始

--- app/controllers/public/recipe_comments_controller.rb
+++ app/controllers/public/recipe_comments_controller.rb
@@ -1,2 +1,22 @@
 class Public::RecipeCommentsController < ApplicationController
+  
+  def create
+    recipe = Recipe.find(params[:recipe_id])
+    comment = current_customer.recipe_comments.new(recipe_comment_params)
+    comment.recipe_id = recipe.id
+    comment.save
+    redirect_to recipe_path(recipe)
+  end
+  
+  def destroy
+    RecipeComment.find(params[:id]).destroy
+    redirect_to recipe_path(params[:recipe_id])
+  end
+
+  private
+
+  def recipe_comment_params
+    params.require(:recipe_comment).permit(:comment)
+  end
+  
 end
