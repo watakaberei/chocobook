c1a4bc993c05b2471f7408381314dad94f7d9c56
EC2 Default User <ec2-user@ip-172-31-30-105.ap-northeast-1.compute.internal>
2023-05-29 14:32:42 +0000

    下書き機能実装開始

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -15,11 +15,22 @@ class Public::RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
-    @recipe.customer_id = current_customer.id
-    if @recipe.save
-      redirect_to recipe_path(@recipe)
+    #投稿ボタンを押した場合
+    if params[:recipe]
+      @recipe.customer_id = current_customer.id
+      if @recipe.save(context: :publicize)
+        redirect_to recipe_path(@recipe)
+      else
+        render :new, alert: "登録できませんでした。お手数ですが、入力内容をご確認のうえ再度お試しください"
+      end
+  
+  #下書きボタンを押した場合
     else
-      render :new
+      if @recipe.update(is_draft: true)
+        redirect_to customer_path(current_customer), notice: "レシピを下書き保存しました！"
+      else
+        render :new, alert: "登録できませんでした。お手数ですが、入力内容をご確認のうえ再度お試しください"
+      end
     end
   end
 
