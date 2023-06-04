a9360af4ab5fd28a32490eea691457d8da1d7c0c
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-06-04 10:24:11 +0000

    admin側でのcustomer実装

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,26 @@
 class Admin::CustomersController < ApplicationController
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  # ストロングパラメータ
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :user_name, :email, :encrypted_password)
+  end
 end
