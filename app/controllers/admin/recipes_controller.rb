9977216464c9d426937f1ed34b964bc8aae7ff28
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-06-05 14:50:39 +0000

    コメント一覧定義

--- app/controllers/admin/recipes_controller.rb
+++ app/controllers/admin/recipes_controller.rb
@@ -1,2 +1,27 @@
 class Admin::RecipesController < ApplicationController
+
+  def index
+    @recipes = Recipe.where(is_draft: false)
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+
+  private
+  # ストロングパラメータ
+  def recipe_params
+    params.require(:recipe).permit(
+      :customer_id,
+      :name,
+      :image,
+      :introduction,
+      :cooktime,
+      :material,
+      :procedure,
+      :is_draft,
+      category_ids:[]
+    )
+  end
 end
