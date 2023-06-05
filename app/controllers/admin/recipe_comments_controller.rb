11590e6bf555501280d10e85e7c19e790a5968e6
EC2 Default User <ec2-user@ip-172-31-30-105.ap-northeast-1.compute.internal>
2023-06-05 14:51:18 +0000

    コメント一覧定義

--- app/controllers/admin/recipe_comments_controller.rb
+++ app/controllers/admin/recipe_comments_controller.rb
@@ -1,2 +1,13 @@
 class Admin::RecipeCommentsController < ApplicationController
+
+  def index
+    @recipe_comments = RecipeComment.all
+  end
+
+  private
+
+  def recipe_comment_params
+    params.require(:recipe_comment).permit(:comment)
+  end
+
 end
