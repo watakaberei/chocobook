e73be62837abf934dab61f2fe6b85a38f624c6e2
watakaberei <126738738+watakaberei@users.noreply.github.com>
2023-06-11 02:19:25 +0000

    admin側のコメント削除　修正

--- app/controllers/admin/recipe_comments_controller.rb
+++ app/controllers/admin/recipe_comments_controller.rb
@@ -4,6 +4,13 @@ class Admin::RecipeCommentsController < ApplicationController
     @recipe_comments = RecipeComment.all
   end
 
+  def destroy
+    comment = RecipeComment.find(params[:id])
+    recipe_id = comment.recipe_id # コメントが所属するレシピのIDを取得
+    comment.destroy
+    redirect_to admin_recipe_path(recipe_id)
+  end
+
   private
 
   def recipe_comment_params
