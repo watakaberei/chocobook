13debbe5c4c4dd0dad16fe0616c77081739c23c8
EC2 Default User <ec2-user@ip-172-31-30-105.ap-northeast-1.compute.internal>
2023-07-01 10:06:52 +0000

    新着レシピとランキング発表表示の修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,8 +1,14 @@
 class Public::HomesController < ApplicationController
 
   def top
+    @categories = Category.all
+    @recipes = params[:name].present? ? Category.find(params[:name]).recipes : Recipe.all
+    @recipes_rank = Recipe.find(RecipeBookmark.group(:recipe_id).order('count(recipe_id) DESC').limit(5).pluck(:recipe_id))
+    @recipes_new = Recipe.all.order(created_at: :desc) && Recipe.where(is_draft: false)
   end
 
   def about
   end
+
+
 end
